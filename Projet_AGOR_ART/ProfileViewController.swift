fde9208a1c274e03b4b2d2792107b6bf72e929b8
Ayite <ayite.folly@gmail.com>
2019-12-04 11:55:57 +0100

    Page profile

---
+++ Projet_AGOR_ART/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Projet_AGOR_ART
+//
+//  Created by ayite  on 04/12/2019.
+//  Copyright © 2019 JMD. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
