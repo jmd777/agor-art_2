e6e9f301f0a16ed81a33757b9128534d7efbd5a4
Gass-Dev <gwenaelle.fouquet@gmail.com>
2019-12-10 15:41:33 +0100

    g

---
+++ Projet_AGOR_ART/DescriptionViewController-Freestyle.swift
@@ -0,0 +1,9 @@
+//
+//  DescriptionViewController-Freestyle.swift
+//  Projet_AGOR_ART
+//
+//  Created by GwenOu on 10/12/2019.
+//  Copyright © 2019 JMD. All rights reserved.
+//
+
+import Foundation
