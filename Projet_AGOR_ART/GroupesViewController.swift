fcd2417f7043e677430351c2ba7b0a9d22b76700
jmd777 <jmichelsept@hotmail.fr>
2019-12-12 17:03:15 +0100

    modifications de 17h02 création de nouveau du segue Tab View Controller avec la Groupes.storyboard

---
+++ Projet_AGOR_ART/GroupesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupesViewController.swift
+//  Projet_AGOR_ART
+//
+//  Created by JMD on 12/12/2019.
+//  Copyright © 2019 JMD. All rights reserved.
+//
+
+import UIKit
+
+class GroupesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
