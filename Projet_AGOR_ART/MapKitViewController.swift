dda822e97e3de8769c054d0fb39051a6508c263c
Gass-Dev <gwenaelle.fouquet@gmail.com>
2019-12-13 01:47:58 +0100

    Gwen Map avec géolocalisation

--- Projet_AGOR_ART/MapKitViewController.swift
+++ Projet_AGOR_ART/MapKitViewController.swift
@@ -2,48 +2,35 @@
 //  MapKitViewController.swift
 //  Projet_AGOR_ART
 //
-//  Created by GwenOu on 12/12/2019.
+//  Created by GwenOu on 13/12/2019.
 //  Copyright © 2019 JMD. All rights reserved.
 //
 
-    // MARK: - Imports
 import UIKit
 import MapKit
 import CoreLocation
 
-    // MARK: - ClassMapKitViewController
-
 class MapKitViewController: UIViewController {
 
-    // MARK: - Outlets
+    @IBOutlet weak var mapView: MKMapView!
     
-    @IBOutlet weak var textFieldForAdress: UITextField!
-    @IBOutlet weak var getDirectionsTapped: UIButton!
-    @IBOutlet weak var map: MKMapView!
-    
-        // MARK: - ViewDidLoad
+    let locationManger = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    // MARK: - Actions
-    
-    @IBAction func getDirectionsTapped(_ sender: UIButton) {
         
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+       
     }
     
-    func getAdress() {
-        let geoCoder = CLGeocoder()
-        geoCoder.geocodeAddressString(textFieldForAdress.text!) { (placemarks, error)
-            in
-            guard let placemarks = placemarks, let location = placemarks.first?.location
-                else {
-                    print("No Location Found")
-                    return
-            }
-            print(location)
-        }
-    }
+
+    /*
+    // MARK: - Navigation
+
+    */
 
 }
--- Projet_AGOR_ART/ViewController.swift
+++ Projet_AGOR_ART/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
@@ -15,7 +17,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
