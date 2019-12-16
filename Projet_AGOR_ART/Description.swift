3426114b34cac60a060b9e23debc5eb290f00880
Gass-Dev <gwenaelle.fouquet@gmail.com>
2019-12-16 15:03:11 +0100

    Gwen Description - Mise en place tableviewcontroller pour faire les sections

--- Projet_AGOR_ART/Description.swift
+++ Projet_AGOR_ART/Description.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class DescriptionViewController: UIViewController {
+class DescriptionViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        
         // Do any additional setup after loading the view.
     }
     
